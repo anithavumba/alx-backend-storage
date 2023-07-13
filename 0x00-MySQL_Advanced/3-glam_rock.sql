c25e87c85755a24b0107074459cae639f4dfd2aa
anithavumba <anithavumba@gmail.com>
2023-07-13 16:36:27 +0000

    backend

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,18 +1,7 @@
--- Create a temporary table to store the lifespan calculation
-CREATE TEMPORARY TABLE temp_bands (
-    band_name VARCHAR(100),
-    formed INT,
-    split INT,
-    lifespan INT
-);
-
--- Populate the temporary table by calculating the lifespan for each band
-INSERT INTO temp_bands (band_name, formed, split, lifespan)
-SELECT band_name, formed, split, (2022 - formed) - split AS lifespan
+-- Old school band
+-- lifespan by band and style
+SELECT band_name,
+	ifnull(split, YEAR(CURDATE())) - formed as lifespan
 FROM metal_bands
-WHERE style = 'Glam rock';
-
--- Retrieve the bands ranked by longevity
-SELECT band_name, lifespan
-FROM temp_bands
+WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
